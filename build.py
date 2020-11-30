3e1e776c2fc498ee905eae700e63fac10604ed93
Benedikt Reinartz <filmor@gmail.com>
2020-11-30 17:50:12 +0100

    Only commit

---
+++ build.py
@@ -0,0 +1,3 @@
+from print_color import print
+
+print("Hello world", tag='success', tag_color='green', color='white')
---
+++ tests/test_issue3428.py
@@ -0,0 +1,5 @@
+from issue3428 import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
